ad0eab60cc6556eb44001643c0e2262e11c50dd9
panwenqi <pwq123_321@163.com>
2014-08-25 23:48:14 +0800

    修改了尾结点指向前一个结点的异常，并且搞清楚了指针本身的值就是一个地址，明天再继续

--- double_list/double_list.c
+++ double_list/double_list.c
@@ -4,6 +4,7 @@
 PNode create_node(Type p_data)  //创建一个结点
 {
 	PNode temp = (PNode)malloc(sizeof(Node));
+	printf("%p\n", &temp);
 	if (temp != NULL)
 	{
 		temp->front = NULL;
@@ -52,9 +53,18 @@ void insert_h_list(PNode head, PNode tail, Type p_data)  //头插法
 		if (size == NULL)
 		{
 			temp->front = tail->front;
-			temp->next = head->next;
+
+			/* 注意了，这个疑问点终于解决了，你malloc产生的结点，其实本质就是把malloc申请的那段内存的起始地址给了temp
+			   也就是说tail就是代表着尾结点的那段内存起始值，head->next 也是尾结点的内存起始地址
+			   tail也是代表着尾结点的起始地址，使用这两个都行
+			   又说明了一个东西：指针的值就是一个地址，指针本身是一个变量，这个变量也有他自己的地址，但是和他里面存放的数据（地址）
+			   不是同一个地址
+			*/
+
+//			temp->next = head->next; //下面的也可以
+			temp->next = tail;  //上面的也可以
 			head->next = temp;
-			tail->next = temp;
+			tail->front = temp;
 		}else
 		{
 			temp->front = head;
