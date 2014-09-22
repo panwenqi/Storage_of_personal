05783297aac95daa16b610e072b6ee75ccdc40bf
panwenqi <pwq123_321@163.com>
2014-09-22 23:37:37 +0800

    test

--- tinyhttpd-0.1.0/argv.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main()
-{
-	char* argv[] = {"hello", "world"};
-	printf("argv[1] = %s\n", argv[1]);
-	return 0;
-}
--- tinyhttpd-0.1.0/simpleclient.c
+++ tinyhttpd-0.1.0/simpleclient.c
@@ -4,7 +4,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-
+//test
 int main(int argc, char *argv[])
 {
 	int sockfd;
