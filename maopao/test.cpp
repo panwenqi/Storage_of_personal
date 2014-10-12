89e2a4a76e4f959dc471086acd50613c25304001
panwenqi <pwq123_321@163.com>
2014-10-12 12:05:38 +0800

    完成了普通的冒泡排序法，没有bug已经测试通过

--- maopao/test.cpp
+++ maopao/test.cpp
@@ -5,14 +5,18 @@ using std::endl;
 
 void maopao(int ay[], int len)
 {
-	int temp;
+ 	int temp = 0;
+	/*外部的i控制数组中的每次去对比的那个元素
+	j控制的是，上面ay[i]这个元素去和每个元素对比的次数
+	要注意的是 j<len-i-1 这里必须要减一，数组是从0开始的，比如第一次是6，下面ay[j+1]就会产生越界
+	*/
 	for(int i = 0; i < len; i++)
 	{
-		for(int j = 0; j < len - i; j++)
+		for(int j = 0; j < len - i - 1; j++) 
 		{
 			if(ay[j] > ay[j + 1])
 			{
-				temp = ay[j + 1];
+				temp = ay[j];
 				ay[j] = ay[j + 1];
 				ay[j + 1] = temp;
 			}
